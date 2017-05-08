055d0ee32f4f240c8a9cdf12a052af4edad5abea
Cayden <cayden.simler@hotmail.com>
2017-05-08 09:38:11 -0500

    Added introduction to each targeted audience describing site functionality.

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -9,20 +9,38 @@
 	</div>
 	
 	<!-- Pet Owner Introduction -->
-	<div class="col-xs-6 petOwnerBodyHome petOwnerBodyBackground">
+	<div class="col-xs-12 col-sm-6 petOwnerBodyHome petOwnerBodyBackground">
 
 		<div class="col-xs-offset-1 col-xs-10 introductionContentHeader">
-			<div class="introductionHeaderHome">For Pet Owners</div>
+			<div class="col-xs-12 introductionHeaderHome">For Pet Owners</div>
+			<div class="col-xs-12 introductionList">
+				<i class="fa fa-paw" aria-hidden="true"></i> Localized records from all clinic visits.
+				<br>
+				<i class="fa fa-paw" aria-hidden="true"></i> Ability to switch vets easily without having to request records from previous vet clinics.
+				<br>
+				<i class="fa fa-paw" aria-hidden="true"></i> No hastle of keeping folders and receipts of all treatments to your pet(s).
+				<br>
+				<i class="fa fa-paw" aria-hidden="true"></i> Completely paperless.
+			</div>
 		</div>
 
 	</div>
 
 
 	<!-- Vet Clinic Introduction -->
-	<div class="col-xs-6 vetClinicBodyHome vetClinicBodyBackground">
+	<div class="col-xs-12 col-sm-6 vetClinicBodyHome vetClinicBodyBackground">
 
 		<div class="col-xs-offset-1 col-xs-10 introductionContentHeader">
-			<div class="introductionHeaderHome">For Vet Clinics</div>
+			<div class="col-xs-12 introductionHeaderHome">For Vet Clinics</div>
+			<div class="col-xs-12 introductionList">
+				<i class="fa fa-paw" aria-hidden="true"></i> Ability to pull in new patient's records from other clinics.
+				<br>
+				<i class="fa fa-paw" aria-hidden="true"></i> Fully structured list of all patients in your personal database.
+				<br>
+				<i class="fa fa-paw" aria-hidden="true"></i> Backed up files stored in our database.
+				<br>
+				<i class="fa fa-paw" aria-hidden="true"></i> Completely paperless.
+			</div>
 		</div>
 	</div>
 
