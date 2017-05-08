d798160fdb32d4670b0786f117eb1f04432b3789
Cayden <cayden.simler@hotmail.com>
2017-05-08 09:10:33 -0500

    Added view for the homepage, and started structuring the overall page look. Included a master.blade for linking css files as well as any consistent code that's going to be used throughout the site such as bootstrap link and google fonts link.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>HealthEPet</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Google Font Open Sans -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+
+		<!-- Personalized Stylesheet -->
+        <link rel="stylesheet" href="/css/main.css">
+
+    </head>
+    <body>
+
+
+
+    @yield('content')
+
+
+
+
+    </body>
+</html>
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,43 +3,26 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
             body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
+                font-family: 'Open Sans Condensed', sans-serif;
+                font-size: 6vw;
+                background-color: #EFEFEF;
+                color: #638DA5;
             }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
+        
+
+
+
+        Laravel 53423 !?#*(&)
+
+
+
+
     </body>
 </html>
