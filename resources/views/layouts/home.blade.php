20962bf84d882f44b3724976c8d8137e0b76b084
Cayden <cayden.simler@hotmail.com>
2017-05-08 11:34:04 -0500

    Added forms for Vet Clinic Login and Vet Clinic Registration.

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -95,8 +95,8 @@
 					<input type="password" name="petOwnerLoginPassword" id="petOwnerLoginPassword" class="formInput">
 				</div>
 
-				<div class="col-xs-offset-3 col-xs-6 formSubmit">
-					<button type="submit" class="petOwnerLoginButton">Login</button>
+				<div class="col-xs-12 loginRegisterButtonBody">
+					<button type="submit" class="petOwnerLoginButton btn btn-default loginRegisterButton">Login</button>
 				</div>
 
 			</form>
@@ -161,8 +161,8 @@
 					</div>
 				</div>
 
-				<div class="col-xs-offset-3 col-xs-6 formSubmit">
-					<button type="submit" class="petOwnerLoginButton">Register</button>
+				<div class="col-xs-12 loginRegisterButtonBody">
+					<button type="submit" class="petOwnerRegisterButton btn btn-default loginRegisterButton">Register</button>
 				</div>
 
 			</form>
@@ -174,13 +174,84 @@
 	<!-- Vet Clinic Login Form -->
 	<div class="vetClinicLoginForm" style="display: none;">
 		<div class="col-xs-offset-2 col-xs-8 col-sm-offset-3 col-sm-6 formBackground">
+			<form action="" method="POST">
+				<div class="col-xs-12 closeButton">
+					<div class="col-xs-8">Vet Clinic Login</div>
+					<i class="fa fa-window-close" aria-hidden="true"></i>
+				</div>
 
+				<div class="col-xs-offset-3 col-xs-6 formLabel">Email</div>
+				<div class="col-xs-offset-3 col-xs-6">
+					<input type="text" name="vetClinicLoginEmail" id="vetClinicLoginEmail" class="formInput">
+				</div>				
+
+				<div class="col-xs-offset-3 col-xs-6 formLabel">Password</div>
+				<div class="col-xs-offset-3 col-xs-6">
+					<input type="password" name="vetClinicLoginPassword" id="vetClinicLoginPassword" class="formInput">
+				</div>
+
+				<div class="col-xs-12 loginRegisterButtonBody">
+					<button type="submit" class="vetClinicLoginButton btn btn-default loginRegisterButton">Login</button>
+				</div>
+
+			</form>
 		</div>
 	</div>
+
 
 	<!-- Vet Clinic Registration Form -->
 	<div class="vetClinicRegisterForm" style="display: none;">
 		<div class="col-xs-offset-2 col-xs-8 col-sm-offset-3 col-sm-6 formBackground">
+
+			<form action="" method="POST">
+				<div class="col-xs-12 closeButton">
+					<div class="col-xs-8">Vet Clinic Registration</div>
+					<i class="fa fa-window-close" aria-hidden="true"></i>
+				</div>
+
+				<div class="col-xs-12 formLabel">Clinic Name</div>
+				<div class="col-xs-12">
+					<input type="text" name="vetClinicName" id="vetClinicName"class="formInput">
+				</div>	
+
+				<div class="col-xs-6">
+					<div class="col-xs-12 formLabel">Email Address</div>
+					<div class="col-xs-12">
+						<input type="text" name="vetClinicRegisterEmail" id="vetClinicRegisterEmail" class="formInput">
+					</div>	
+				</div>						
+
+				<div class="col-xs-6">
+					<div class="col-xs-12 formLabel">Phone Number</div>
+					<div class="col-xs-12">
+						<input type="text" name="vetClinicRegisterPhone" id="petOwnerRegisterPhone" class="formInput">
+					</div>
+				</div>	
+
+				<div class="col-xs-12 formLabel">Physical Address</div>
+				<div class="col-xs-12">
+					<input type="text" name="vetClinicRegisterAddress" id="vetClinicRegisterAddress" placeholder="123 Pet Owner St, Paradise City, Texas, 12345" class="formInput">
+				</div>
+
+				<div class="col-xs-6">
+					<div class="col-xs-12 formLabel">Password</div>
+					<div class="col-xs-12">
+						<input type="password" name="vetClinicRegisterPassword" id="vetClinicRegisterPassword" class="formInput">
+					</div>
+				</div>
+
+				<div class="col-xs-6">
+					<div class="col-xs-12 formLabel">Confirm Password</div>
+					<div class="col-xs-12">
+						<input type="password" name="vetClinicRegisterConfirmPassword" id="vetClinicRegisterConfirmPassword" class="formInput">
+					</div>
+				</div>
+
+				<div class="col-xs-12 loginRegisterButtonBody">
+					<button type="submit" class="vetClinicRegisterButton btn btn-default loginRegisterButton">Register</button>
+				</div>
+
+			</form>
 
 		</div>
 	</div>
