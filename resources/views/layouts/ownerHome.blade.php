5b20753dc5cbb901dd5478caef139e5083cdeee1
Cayden <cayden.simler@hotmail.com>
2017-05-09 10:49:58 -0500

    Modified homepage for a cleaner style, switched to select options for birth year in the add new pet form.

--- resources/views/layouts/ownerHome.blade.php
+++ resources/views/layouts/ownerHome.blade.php
@@ -8,14 +8,17 @@
 
 
 
-	<div class="welcomeHeader">
-		Welcome to your account page, $user->firstName.
-	</div>
 
 	<div class="allContent">
+
+		<div class="welcomeHeader">
+			Welcome to your account page, $user->firstName.
+		</div>
+
 		<div class="col-xs-offset-1 col-xs-10">
 			<button class="btn btn-default addPetButton"><i class="fa fa-plus-square" aria-hidden="true"></i> Add New Pet</button>
 		</div>
+
 	</div>
 
 
@@ -75,7 +78,7 @@
 				</div>	
 
 				<div class="col-xs-6">
-					<div class="col-xs-12 formLabel">Weight</div>
+					<div class="col-xs-12 formLabel">Weight (pounds)</div>
 					<div class="col-xs-12">
 						<input type="text" name="weight" id="weight" class="formInput" required>
 					</div>
@@ -84,7 +87,23 @@
 				<div class="col-xs-6">
 					<div class="col-xs-12 formLabel">Birth Year</div>
 					<div class="col-xs-12">
-						<input type="text" name="birthYear" id="birthYear" class="formInput" required>
+						<!-- <input type="text" name="birthYear" id="birthYear" class="formInput" required> -->
+						<select name="birthYear" id="birthYear" class="birthYearDropDown">
+							<option value="2000">2000</option>
+							<option value="2001">2001</option>
+							<option value="2002">2002</option>
+							<option value="2003">2003</option>
+							<option value="2004">2004</option>
+							<option value="2005">2005</option>
+							<option value="2006">2006</option>
+							<option value="2007">2007</option>
+							<option value="2008">2008</option>
+							<option value="2009">2009</option>
+							<option value="2010">2010</option>
+							<option value="2011">2011</option>
+							<option value="2012">2012</option>
+							<option value="2013">2013</option>
+						</select>
 					</div>
 				</div>						
 
