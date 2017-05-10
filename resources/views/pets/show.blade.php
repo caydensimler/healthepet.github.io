331273de448056f6d31f2d3bb1c76dea449a65ee
Thomas Hayes <thomashayes7889@gmail.com>
2017-05-10 09:54:56 -0500

    created basic view of the show page

--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-	<h1>{{ $pet->name}}</h1>
-	    <p>{{ $pet->breed }}</p>
-        <p>{{ $pet->sex }}</p>
-        <p>{{ $pet->age }} </p>    
-        <p>{{ $pet->owner_id }}  </p>     
-        <p>{{ $pet->vet_id }}</p>
+	<div class="container col-sm-12">
+		<h1 class="text-center">{{ $pet->name}}</h1>
+		<table class="table">
+			<tr>
+				<th class="col-sm-3">Shot Name:</th>
+				<th class="col-sm-3"></a>Received Shot:</th>
+				<th class="col-sm-3">Date Administered:</th>
+				<th class="col-sm-3">Renew Date:</th>
+				<th class="col-sm-3">Comments:</th>
+			</tr>
+			
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+			</tr>
+			
+		</table>
+		
+	</div>
 
 @stop
