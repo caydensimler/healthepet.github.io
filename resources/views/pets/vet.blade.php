1cc3d46158f7f94cfd582b5cc9441bbd87afae37
Cayden <cayden.simler@hotmail.com>
2017-05-11 14:20:35 -0500

    Modified view for vet's patients.

--- resources/views/pets/vet.blade.php
+++ resources/views/pets/vet.blade.php
@@ -13,50 +13,48 @@
 
 	<div class="col-xs-12">
 		@foreach($pets as $pet)
-			<div class="col-xs-6 petBody">
+			<div class="col-xs-12 col-md-6 petBody">
 				<div class="col-xs-12 petNameVet">{{ $pet->name }}</div>
 
-				<div class="col-xs-6">
-					<div class="col-xs-12 petRecordSubHeader">Pet Information</div>
-
+				<div class="col-xs-12">
 
 					<div class="col-xs-6 petRecordInformation">
-						<div class="col-xs-3">Species:</div>
-						<div class="col-xs-9">{{ $pet->species }}</div>
+						<div class="col-xs-4">Species:</div>
+						<div class="col-xs-8">{{ $pet->species }}</div>
 					</div>					
 
 					<div class="col-xs-6 petRecordInformation">
-						<div class="col-xs-3">Breed:</div>
-						<div class="col-xs-9">{{ $pet->breed }}</div>
+						<div class="col-xs-4">Breed:</div>
+						<div class="col-xs-8">{{ $pet->breed }}</div>
 					</div>					
 
 					<div class="col-xs-6 petRecordInformation">
-						<div class="col-xs-3">Sex:</div>
-						<div class="col-xs-9">{{ $pet->sex }}</div>
+						<div class="col-xs-4">Sex:</div>
+						<div class="col-xs-8">{{ $pet->sex }}</div>
 					</div>					
 
 					<div class="col-xs-6 petRecordInformation">
-						<div class="col-xs-3">Weight:</div>
-						<div class="col-xs-9">{{ $pet->weight }}</div>
+						<div class="col-xs-4">Weight(lbs):</div>
+						<div class="col-xs-8">{{ $pet->weight }}</div>
 					</div>					
 
 					<div class="col-xs-6 petRecordInformation">
-						<div class="col-xs-3">Color(s)</div>
-						<div class="col-xs-9">{{ $pet->color }}</div>
+						<div class="col-xs-4">Color(s)</div>
+						<div class="col-xs-8">{{ $pet->color }}</div>
 					</div>					
 
 					<div class="col-xs-6 petRecordInformation">
-						<div class="col-xs-3">Age (years):</div>
-						<div class="col-xs-9">{{ $pet->age }}</div>
+						<div class="col-xs-4">Age(years):</div>
+						<div class="col-xs-8">{{ $pet->age }}</div>
 					</div>
 
 				</div>
-				<div class="col-xs-6">
+				<!-- <div class="col-xs-6">
 					<div class="col-xs-12 petRecordSubHeader">Owner Information</div>
 					<div class="col-xs-12">{{ $pet->user->name }}</div>
 					<div class="col-xs-12">{{ $pet->user->phoneNumber }}</div>
 					<div class="col-xs-12">{{ $pet->user->email }}</div>
-				</div>
+				</div> -->
 			</div> 
 		@endforeach
 	</div>
