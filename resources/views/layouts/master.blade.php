ca97eef7c05f184f88610af5f43a20aeabfdf168
Cayden <cayden.simler@hotmail.com>
2017-05-11 16:00:00 -0500

    Finished form for editing user account.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -28,9 +28,10 @@
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        <li class="siteName hidden-xs">HealthEPet</li>
-                        <li><a href="{{ action('UsersController@show', [$user->id]) }}">Account</a></li>
-                        <li><a href="{{ action('PetsController@index') }}">Pets</a></li>
+                        <li>
+                            <a href="{{ action('PetsController@index') }}">HealthEPet</a>
+                        </li>
+                        <li><a href="{{ action('UsersController@show') }}">Account</a></li>
                         <li><a href="/auth/logout">Logout</a></li>
                     </ul>
                 </div>
--- resources/views/users/userAccount.blade.php
+++ resources/views/users/userAccount.blade.php
@@ -2,15 +2,65 @@
 
 @section('content')
 
-	<div class="container col-sm-12">
-		<h1>Account Info</h1>
-		<h4>Name: </h4><p>{{ $user->name }}</p>
-		<h4>Email: </h4><p>{{ $user->email }}</p>
-		<h4>Password:</h4><p>**********</p>
-		<h4>Last Updated: </h4><p>{{ $user->updated_at }}</p>
-		
-		
-    	<a href="{{ action('UsersController@edit', [$user->id]) }}" class="col-sm-3 btn btn-primary">Edit Account </a>
-    	
+	<div class="col-xs-12 welcomeHeaderAccountPage">
+		{{ $user->name }}'s Account Information
+	</div>
+
+
+	<div class="col-xs-offset-1 col-xs-10 col-md-offset-3 col-md-6 formBackgroundAccount">
+		<form action="{{ action('UsersController@update', [$user->id]) }}" method="POST">
+		{!! csrf_field() !!}
+
+			<div class="col-xs-12 accountFormHeader">Edit Account Information</div>
+
+			<div class="col-xs-12 col-md-6">
+				<div class="col-xs-12 formLabelAccount">Name</div>
+				<div class="col-xs-12">
+					<input class="formInputAccount" type="text" name="name" value="{{ $user->name }}" placeholder="Name" required>
+				</div>
+			</div>		
+
+			<div class="col-xs-12 col-md-6">
+				<div class="col-xs-12 formLabelAccount">Phone Number</div>
+				<div class="col-xs-12">
+					<input class="formInputAccount" type="text" name="phoneNumber" value="{{ $user->phoneNumber }}" placeholder="Phone Number" required>
+				</div>
+			</div>
+
+			<div class="col-xs-12">
+				<div class="col-xs-12 formLabelAccount">Email Address</div>
+				<div class="col-xs-12">
+					<input class="formInputAccount" type="text" name="email" value="{{ $user->email }}" placeholder="Email Address" required>
+				</div>
+			</div>		
+
+			<div class="col-xs-12">
+				<div class="col-xs-12 formLabelAccount">Physical Address</div>
+				<div class="col-xs-12">
+					<input class="formInputAccount" type="text" name="address" value="{{ $user->address }}" placeholder="Physical Address" required>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-md-6">
+				<div class="col-xs-12 formLabelAccount">Password</div>
+				<div class="col-xs-12">
+					<input class="formInputAccount" type="password" name="password" placeholder="**************">
+				</div>
+			</div>		
+
+			<div class="col-xs-12 col-md-6">
+				<div class="col-xs-12 formLabelAccount">Confirm Password</div>
+				<div class="col-xs-12">
+					<input class="formInputAccount" type="confirmPassword" name="confirmPassword" placeholder="**************">
+				</div>
+			</div>
+
+			{{ method_field('PUT') }}
+			<input type="hidden" name="id" value="{{Auth::id()}}">
+			<div class="col-xs-12 loginRegisterButtonBody">
+				<input type="submit" value="Update User" class="btn btn-default editAccountButton">
+			</div>
+
+		</form>
     </div>
 @stop
