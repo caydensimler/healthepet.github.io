09bd133f271d1d4a33766b406abe4170742ec81a
Cayden <cayden.simler@hotmail.com>
2017-05-15 09:51:45 -0500

    Modified view pages for the vets information page and the pet owners list of pets.

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -17,54 +17,45 @@
         
 		@foreach($pets as $pet)
 
-			<div class="col-xs-12 petInformation">
+			<div class="col-xs-12 col-sm-6 petInformation">
 
-				<div class="col-xs-3"><img class="petHeaderPicture" src="/img/<?php if ($pet->img === null) { echo 'sampleDogPicture.jpg'; } else { echo '{{ $pet->img }}'; }  ?>" alt="dog/cat picture"></div>
+                <div class="col-xs-offset-1 col-xs-3">
+                    <img class="petHeaderPicture" src="/img/<?php if ($pet->img == null) { echo 'sampleDogPicture.jpg'; } else { echo '{{ $pet->img }}'; }  ?>" alt="dog/cat picture">
+                </div>
 
-				<div class="col-xs-offset-1 col-xs-8 petDescriptionBody">
+				<div class="col-xs-offset-2 col-xs-5 petDescriptionBody">
 
-                    <!-- <div class="col-xs-12 petNameHeader"> -->
-                        <div class="col-xs-5 petNameVet">{{ $pet->name }}</div>
-                        <div class="col-xs-7 recordsLink">
-                            <a href="/pets/{{ $pet->id }}">Click here to view {{ $pet->name }}'s records. <i class="fa fa-file-text-o" aria-hidden="true"></i></a>
-                        </div>
-                    <!-- </div> -->
+                    <div class="col-xs-12 petNameVet">{{ $pet->name }}</div>
 
-					<div class="col-xs-6">
-						<div class="col-xs-12 petSubHeader">
-							Species: {{ $pet->species }}
-						</div>					
+					<div class="col-xs-12 petSubHeader">
+						Species: {{ ucfirst($pet->species) }}					
+                        <br>
+						Breed: {{ $pet->breed }}					
+                        <br>
+						Sex: {{ ucfirst($pet->sex) }}
+                        <br>
+						Weight: {{ $pet->weight }} pounds
+                        <br>					
+						Color(s): {{ $pet->color }}		
+                        <br>			
+						Age: {{ (date('Y') - $pet->age) }} years
+                        <br>
+                    </div>
 
-						<div class="col-xs-12 petSubHeader">
-							Breed: {{ $pet->breed }}
-						</div>					
-
-						<div class="col-xs-12 petSubHeader">
-							Gender: {{ $pet->sex }}
-						</div>
+                    <div class="col-xs-6">
 					</div>
 
-					<div class="col-xs-6">
-						<div class="col-xs-12 petSubHeader">
-							Weight: {{ $pet->weight }}
-						</div>					
-
-						<div class="col-xs-12 petSubHeader">
-							Color(s): {{ $pet->color }}
-						</div>					
-
-						<div class="col-xs-12 petSubHeader">
-							Age: {{ $pet->age }}
-						</div>
-					</div>
-
-                    <div class="col-xs-12">
+<!--                     <div class="col-xs-12">
                         <div class="col-xs-offset-1 col-xs-10 addEditPicture">
                             Add/Edit Picture
                         </div>
-                    </div>
+                    </div> -->
 
 				</div>
+    
+                <div class="col-xs-12 recordsLink">
+                    <a href="/pets/{{ $pet->id }}">Click here to view {{ $pet->name }}'s records. <i class="fa fa-file-text-o" aria-hidden="true"></i></a>
+                </div>
 
 			</div>
 		@endforeach
