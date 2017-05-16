f6f35d0556cfd6dac87f8114a1d2004212f5c45b
Cayden <cayden.simler@hotmail.com>
2017-05-16 11:26:48 -0500

    Fixed styling for show.blade and index.blade

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -30,29 +30,50 @@
 
                 <div class="col-xs-offset-1 col-xs-3">
                     <img class="petHeaderPicture" src="/img/{{ $pet->img == null ? 'default.png' : $pet->img }}" alt="dog/cat picture">
-                    <form action="{{ action('PetsController@image', $pet->id) }}" method="post" enctype="multipart/form-data">
-                    {!! csrf_field() !!}
-                        Add/Edit Image:
-                        <input type="hidden" name="pet_id" value="{{ $pet->id }}">
-                        <input type="file" name="fileToUpload" id="fileToUpload">
-                        <input type="submit" value="Upload Image" name="submit">
-                    </form>
-                </div>	
 
-				<div class="col-xs-offset-2 col-xs-5 petDescriptionBody">
+                </div>  
+
+                <div class="col-xs-offset-2 col-xs-5 petDescriptionBody">
 
                     <div class="col-xs-12 petNameVet">{{ $pet->petName }}</div>
 
-                    <div class="col-xs-12 vetRecordsLink">
-                        <a href="/pets/{{ $pet->id }}">Click here to view records. <i class="fa fa-file-text-o" aria-hidden="true"></i></a>
-                    </div>
+                    
+                    <form action="{{ action('PetsController@image', $pet->id) }}" method="post" enctype="multipart/form-data">
+                    {!! csrf_field() !!}
+                        <div class="addImage col-xs-12">Upload an image of your pet!</div>
 
-				</div>
+                        <input type="hidden" name="pet_id" value="{{ $pet->id }}">
+
+                        <div class="col-xs-12">
+                            <label for="fileToUpload" class="chooseFile">
+                                <input type="file" name="fileToUpload" id="fileToUpload" class="hiddenInputFileUpload">Choose File
+                            </label>
+                        </div>
+
+                        <div class="col-xs-12">
+                            <div class="col-xs-12 fileUploadLink"></div>
+                        </div>
+
+                        <div class="col-xs-12">
+                            <label for="submit" class="chooseFile">
+                                <input type="submit" value="Upload Image" name="submit" id="submit" hidden>Upload Image
+                            </label>
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div class="col-xs-12 petOwnerRecordsLink">
+                    <a href="/pets/{{ $pet->id }}">Click here to view Loca's records. <i class="fa fa-file-text-o" aria-hidden="true"></i></a>
+                </div>
 
 			</div>
 		@endforeach
         </div>
 
-    {!! $pets->render() !!}
+        <div class="col-xs-12 paginationHolder">
+          {!! $pets->render() !!}
+        </div>
 
 @stop
--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -8,21 +8,22 @@
 	</div>
 
 	@if (Auth::user()->user_type === 'vet')
-	<div class="col-xs-offset-1 col-xs-10 col-md-offset-4 col-md-4 treatmentTypeButton shotButton">
-		Add Vaccination	
+	<div class="col-xs-offset-5 col-xs-2 loginRegisterButtonBody">
+		<div class="shotButton"><i class="fa fa-plus-square" aria-hidden="true"></i> Add Vaccination</div>
 	</div>
 	@endif
 
 	@if (sizeof($shots) === 0) 
 		<div class="col-xs-12 noRecordsFound">Sorry, no records available! :(</div>
-		<div class="col-xs-offset-4 col-xs-4"><img src="/img/<?php if ($pet->species == 'dog') { echo 'noRecordsDog.jpg'; } else { echo 'noRecordsCat.jpg'; } ?>" alt="No Records Found Sad Puppy" class="noRecordsPicture"></div>
+		<div class="col-xs-offset-4 col-xs-4"><img src="/img/<?php if ($pet->species == 'dog') { echo 'noRecordsDog.jpg'; } else { echo 'noRecordsCat.jpg'; } ?>" alt="No Records Found Sad Pet" class="noRecordsPicture"></div>
 	@elseif (sizeof($shots) != 0)
 		<div class="col-xs-12 shotRecordsTable">
 
 			<div class="col-xs-12 shotRecordsHeaderBody">
-				<div class="col-xs-6 shotRecordsHeader">Vaccination Name</div>
+				<div class="col-xs-3 shotRecordsHeader">Vaccination Name</div>
 				<div class="col-xs-3 shotRecordsHeader">Date Administered</div>
 				<div class="col-xs-3 shotRecordsHeader">Renewal Date</div>
+				<div class="col-xs-3 shotRecordsHeader">Clinic Name</div>
 			</div>
 
 
@@ -30,7 +31,7 @@
 			<?php $i = 0; ?>
 			@foreach ($pet->shots as $shot)
 				<div class="col-xs-12 <?php if ($i % 2 === 0) { echo 'shotRecordsBody'; } else { echo 'shotRecordsBodyAlt' ;} ?>">
-					<div class="col-xs-6 shotRecordsContent">
+					<div class="col-xs-3 shotRecordsContent">
 						{{ $shot->shotName }}
 					</div>
 
@@ -41,6 +42,8 @@
 					<div class="col-xs-3 shotRecordsContent">
 						{{ $shot->pivot->date_renewal }}
 					</div>
+
+					<div class="col-xs-3 shotRecordsContent"></div>
 					<?php $i++ ?>
 				</div>
 			@endforeach
